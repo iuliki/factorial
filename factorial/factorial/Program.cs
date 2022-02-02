c71397218abba00757385fc0bc275091ad935905
iuliki <iulia.bulugu@emanuel.ro>
2022-02-02 18:44:02 +0200

    factorial1 - done

---
+++ factorial/factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Factorial(4));
+        }
+
+        public static int Factorial(int n)
+        {
+            if (n == 1)
+                return 1;
+            else
+                return n * Factorial(n - 1);
+        }
+    }
+}
